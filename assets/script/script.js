a124abc83da5cf80afdad18e0a9443723c476c4b
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-08-25 00:16:27 +0700

    fix:js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,23 @@
-let writeList = () => {
+//const list = document.getElementById('items');
+
+const writeList = () => {
     const inputValue = document.getElementById('input').value;
-    const list = document.getElementById('items');
-    const newElement = document.createElement('li');
-    newElement.textContent = inputValue;
-    list.append(newElement)
+
+    const items = document.getElementById('items');
+
+    const newItem = document.createElement('li');
+    newItem.textContent = inputValue;
+    items.append(newItem);
+    newItem.classList.add('main__list_items-item');
+
+    document.getElementById('input').value = ''
 }
 writeList()
+
+const list = document.querySelector('ul');
+
+list.addEventListener('click', function(event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('inert');
+    }
+});
